4aee1d797ff342eb8904c1ed63912f0e58e6a664
maganijnr <maganijoshua25@gmail.com>
2022-11-18 11:41:02 +0100

    origin

--- server.js
+++ server.js
@@ -16,7 +16,9 @@ dotenv.config();
 const app = express();
 
 //Middleware
-app.use(cors());
+app.use(
+	cors({ origin: ["http://localhost:3000", "https://eubond-mern-admin"] })
+);
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(cookieParser());
